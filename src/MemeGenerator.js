cb09ec670e2783947ebf96b10c623fee89ccfdb9
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-02-28 19:48:21 +0100

    Completed adding text to image via input

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -2,22 +2,45 @@ import { useState } from "react";
 import data from "./memesData";
 
 export default function MemeGenerator() {
-  const [newMeme, changeMeme] = useState(data.memes[0].url);
-  const handleGetNewMeme = () => {
-    const randomId = Math.floor(Math.random() * data.memes.length);
-    changeMeme(data.memes[randomId].url);  
-  };
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    name: "Index Image",
+    url: data.memes[0].url,
+  });
+  const [allMemeImages, setAllMemeImages] = useState(data)
+  const getNewMeme = () => {
+    const randomId = Math.floor(Math.random() * allMemeImages.memes.length);
+    const url = allMemeImages.memes[randomId].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      url
+    }))
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        [name]: value
+      }
+    })
+  }
   return (
     <div className="content-body">
       <div className="input-group">
-        <input type="text" />
-        <input type="text" />
+        <input name="topText" value={meme.topText} onChange={handleChange} type="text" placeholder="Top text" />
+        <input name="bottomText" value={meme.bottomText} onChange={handleChange} type="text" placeholder="Bottom text" />
       </div>
-      <button onClick={() => handleGetNewMeme()}>
+      <button onClick={() => getNewMeme()}>
         Get a new meme image 😎
       </button>
       <div className="meme-img">
-        <img src={newMeme} />
+        <div className="top-text meme-text">{meme.topText}</div>
+        <div className="bottom-text meme-text">{meme.bottomText}</div>
+        <img src={meme.url} alt={meme.name} />
+
       </div>
     </div>
   );
