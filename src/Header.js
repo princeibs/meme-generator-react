e7425ba5e43f937ea70c431d3be57039c6a4f1d9
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-03-01 20:58:03 +0100

    Update icon image

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
+import memeIcon from "./meme_icon.ico"
+
 export default function Header() {
   return (
     <div className="header">
       <ul>
         <li>
-          <img src="https://picsum.photos/id/27/200/300" alt="" />
+          <img src={memeIcon} alt="Logo" />
         </li>
         <li className="content-title">Meme Generator</li>
       </ul>
